f067021f74c238cc162a0b3107469965f59f9e47
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-02-13 14:01:25 +0800

    update & add tests

--- swift/Tests/AlibabaCloudCommonsTests/AlibabaCloudCommonsTests.swift
+++ swift/Tests/AlibabaCloudCommonsTests/AlibabaCloudCommonsTests.swift
@@ -49,9 +49,70 @@ final class AlibabaCloudCommonsTests: XCTestCase {
     }
 
     func testGetNonce() {
-        let str:String = AlibabaCloudCommons.getNonce()
-        print(str)
-        print(str.lengthOfBytes(using: .utf8))
+        XCTAssertEqual(32, AlibabaCloudCommons.getNonce().lengthOfBytes(using: .utf8))
+    }
+
+    func testGetSignature() {
+        let secret: String = "access-key-secret"
+        let request = TeaRequest()
+        request.query["foo"] = "bar"
+        XCTAssertEqual("LYCzlHWLR8dc/gOmv9u5dEfpPvU=", AlibabaCloudCommons.getSignature(request, secret))
+    }
+
+    func testJson() {
+        let sm: SessionManager = Alamofire.SessionManager(configuration: URLSessionConfiguration.default)
+        let queue: DispatchQueue = DispatchQueue(label: "AlibabaCloudCommonsTests.TestsQueue")
+        let promise = sm.request("https://httpbin.org/get?foo=bar", method: HTTPMethod.get).response(queue: queue)
+        do {
+            let response: DefaultDataResponse = try await(promise)
+            let teaResponse: TeaResponse = TeaResponse(response)
+            let dict = AlibabaCloudCommons.json(teaResponse)
+            let args: [String: String] = dict["args"] as! [String: String]
+            XCTAssertEqual(args["foo"], "bar")
+        } catch {
+            XCTAssertTrue(false)
+        }
+    }
+
+    func testHasError() {
+        var dict: [String: AnyObject] = [String: AnyObject]()
+
+        XCTAssertTrue(AlibabaCloudCommons.hasError())
+        XCTAssertFalse(AlibabaCloudCommons.hasError(dict))
+
+        dict["Code"] = "Success" as AnyObject
+        XCTAssertTrue(AlibabaCloudCommons.hasError(dict))
+    }
+
+    func testGetTimestamp() {
+        let now = AlibabaCloudCommons.getTimestamp()
+        sleep(1)
+        let end = AlibabaCloudCommons.getTimestamp()
+
+        XCTAssertTrue(end - now >= 0)
+    }
+
+    func testToForm() {
+        let data: Data = "string".data(using: .utf8)!
+        XCTAssertEqual("".data(using: .utf8), AlibabaCloudCommons.toForm(dict: nil, data, "boundary"))
+        var dict: [String: AnyObject] = [String: AnyObject]()
+        dict["foo"] = "bar" as AnyObject
+
+        var result: Data = AlibabaCloudCommons.toForm(dict: dict, data, "boundary")
+        var str: String = String(data: result, encoding: .utf8) ?? ""
+        XCTAssertEqual(str, "--boundary\r\nContent-Disposition: form-data; name=\"x-oss-meta-foo\"\r\n\r\nbar\r\n--boundary--\r\n")
+
+        var headerFile: [String: AnyObject] = [String: AnyObject]()
+        headerFile["content-type"] = "json" as AnyObject
+        headerFile["filename"] = "filename" as AnyObject
+        headerFile["content"] = "content" as AnyObject
+        var userMeta: [String: String] = [String: String]()
+        userMeta["test"] = "test"
+        dict["file"] = headerFile as AnyObject
+        dict["UserMeta"] = userMeta as AnyObject
+        result = AlibabaCloudCommons.toForm(dict: dict, data, "boundary")
+        str = String(data: result, encoding: .utf8) ?? ""
+        XCTAssertEqual(str, "--boundary\r\nContent-Disposition: form-data; name=\"x-oss-meta-test\"\r\n\r\ntest\r\n--boundary\r\nContent-Disposition: form-data; name=\"x-oss-meta-foo\"\r\n\r\nbar\r\n--boundary\r\nContent-Disposition: form-data; name=\"file\"; filename=\"filename\"\r\n\r\nContent-Type: jsoncontent\r\n--boundary--\r\n")
     }
 
     static var allTests = [
