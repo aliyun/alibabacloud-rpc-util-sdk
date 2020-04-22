1f6a4249d89c2b61baab90313e78f439ee656dca
824750130 <e824750130@qq.com>
2020-04-22 15:28:51 +0800

    fix: modify code style

--- python/rpc_util/client.py
+++ python/rpc_util/client.py
@@ -9,100 +9,97 @@ from _io import TextIOWrapper
 from urllib.parse import quote_plus
 from Tea.model import TeaModel
 
-class Client:
-    SEPARATOR = "&"
+SEPARATOR = "&"
 
-    @staticmethod
-    def get_endpoint(endpoint, serverUse, endpointType):
-        if endpointType == "internal":
-            str_split = endpoint.split('.')
-            str_split[0] += "-internal"
-            endpoint = ".".join(str_split)
-        
-        if serverUse and endpointType == "accelerate":
-            return "oss-accelerate.aliyuncs.com"
 
-        return endpoint
+def get_endpoint(endpoint, server_use, endpoint_type):
+    if endpoint_type == "internal":
+        str_split = endpoint.split('.')
+        str_split[0] += "-internal"
+        endpoint = ".".join(str_split)
 
-    @staticmethod
-    def get_host(productId, regionId, endpoint):
-        if not endpoint:
-            serviceCode = productId.split('_')[0].lower()
-            return "{}.{}.aliyuncs.com".format(serviceCode,regionId)
+    if server_use and endpoint_type == "accelerate":
+        return "oss-accelerate.aliyuncs.com"
 
-        return endpoint
+    return endpoint
 
-    @staticmethod
-    def get_signature(request, secret):
-        queries = request.query.copy()
-        keys = list(queries.keys())
-        keys.sort()
 
-        canonicalizedQueryString = ""
+def get_host(product_id, region_id, endpoint):
+    if not endpoint:
+        service_code = product_id.split('_')[0].lower()
+        return "{}.{}.aliyuncs.com".format(service_code, region_id)
 
-        for k in keys:
-            if queries[k]:
-                canonicalizedQueryString += "&"
-                canonicalizedQueryString += quote_plus(k,encoding="utf-8")
-                canonicalizedQueryString += "="
-                canonicalizedQueryString += quote_plus(queries[k], encoding="utf-8")
+    return endpoint
 
-        stringToSign = ""
-        stringToSign += request.method
-        stringToSign += Client.SEPARATOR
-        stringToSign += quote_plus("/", encoding="utf-8")
-        stringToSign += Client.SEPARATOR
-        stringToSign += quote_plus(canonicalizedQueryString[1:] if canonicalizedQueryString.__len__() > 0 else canonicalizedQueryString, encoding="utf-8")
-        print("Alibabacloud.Common.GetSignature:stringToSign is " + stringToSign)
-        digest_maker = hmac.new(bytes(secret + Client.SEPARATOR, encoding="utf-8"), bytes(stringToSign, encoding="utf-8"), digestmod=hashlib.sha1)
-        hash_bytes = digest_maker.digest()
-        signedStr = str(base64.b64encode(hash_bytes),encoding="utf-8")
 
-        return signedStr
+def get_signature(request, secret):
+    queries = request.query.copy()
+    keys = list(queries.keys())
+    keys.sort()
 
-    @staticmethod
-    def has_error(obj):
-        if obj is None:
-            return True
-        resultCode = obj.get("Code")
-        if resultCode is None:
-            return False
-        try:
-            return int(resultCode) > 0
-        except ValueError:
-            return True
+    canonicalized_query_string = ""
 
-    @staticmethod
-    def get_timestamp():
-        return datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
-    
-    @staticmethod
-    def convert(body, content):
-        prop_list = [(p, not callable(getattr(body, p)) and p[0] != "_")
-                     for p in dir(body)]
-        pros = {}
-        for i in prop_list:
-            if i[1]:
-                val = getattr(body, i[0])
-                if not isinstance(val, TextIOWrapper):
-                    pros[body._names.get(i[0]) or i[0]] = val if not isinstance(val, TeaModel) else val.to_map()
-                
-        json_body = json.dumps(pros)
-        dic = json.loads(json_body)
-        content.to_object(dic)
+    for k in keys:
+        if queries[k]:
+            canonicalized_query_string += "&"
+            canonicalized_query_string += quote_plus(k,encoding="utf-8")
+            canonicalized_query_string += "="
+            canonicalized_query_string += quote_plus(queries[k], encoding="utf-8")
 
-    @staticmethod
-    def query(dic):
-        out_dic = {}
-        for k in dic:
-            out_dic[k] = str(dic[k]) if dic[k] is not None else ""
-        return out_dic
+    string_to_sign = ""
+    string_to_sign += request.method
+    string_to_sign += SEPARATOR
+    string_to_sign += quote_plus("/", encoding="utf-8")
+    string_to_sign += SEPARATOR
+    string_to_sign += quote_plus(canonicalized_query_string[1:] if canonicalized_query_string.__len__() > 0 else canonicalizedQueryString, encoding="utf-8")
+    print("Alibabacloud.Common.GetSignature:stringToSign is " + string_to_sign)
+    digest_maker = hmac.new(bytes(secret + SEPARATOR, encoding="utf-8"), bytes(string_to_sign, encoding="utf-8"), digestmod=hashlib.sha1)
+    hash_bytes = digest_maker.digest()
+    signed_str = str(base64.b64encode(hash_bytes),encoding="utf-8")
 
-    @staticmethod
-    def get_open_plat_form_endpoint(endpoint, regionId):
-        supportRegionId = ("ap-southeast-1", "ap-northeast-1", "eu-central-1", "cn-hongkong", "ap-south-1")
-        if regionId.lower() in supportRegionId:
-            str_split = endpoint.split(".")
-            str_split[0] = str_split[0] + "." + regionId
-            return ".".join(str_split)
-        return endpoint
+    return signed_str
+
+
+def has_error(obj):
+    if obj is None:
+        return True
+    result_code = obj.get("Code")
+    if result_code is None:
+        return False
+    try:
+        return int(result_code) > 0
+    except ValueError:
+        return True
+
+
+def get_timestamp():
+    return datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
+def convert(body, content):
+    prop_list = [(p, not callable(getattr(body, p)) and p[0] != "_")
+                 for p in dir(body)]
+    pros = {}
+    for i in prop_list:
+        if i[1]:
+            val = getattr(body, i[0])
+            if not isinstance(val, TextIOWrapper):
+                pros[body._names.get(i[0]) or i[0]] = copy.deepcopy(val if not isinstance(val, TeaModel) else val.to_map())
+
+    content.from_map(pros)
+
+
+def query(dic):
+    out_dic = {}
+    for k in dic:
+        out_dic[k] = str(dic[k]) if dic[k] is not None else ""
+    return out_dic
+
+
+def get_open_plat_form_endpoint(endpoint, region_id):
+    support_region_id = ("ap-southeast-1", "ap-northeast-1", "eu-central-1", "cn-hongkong", "ap-south-1")
+    if region_id.lower() in support_region_id:
+        str_split = endpoint.split(".")
+        str_split[0] = str_split[0] + "." + region_id
+        return ".".join(str_split)
+    return endpoint
--- python/tests/test_client.py
+++ python/tests/test_client.py
@@ -1,40 +1,72 @@
 import unittest
 import os
 
-from rpc_util.client import Client
+from rpc_util import client
 from Tea.request import TeaRequest
 from Tea.model import TeaModel
 
+
 class TestClient(unittest.TestCase):
+    class _TeaModel(TeaModel):
+        _base_type = {int, float, bool, complex, str}
+        _list_type = {list, tuple, set}
+        _dict_type = {dict}
 
-    class TestConvertModel(TeaModel):
-        def __init__(self):
-                super().__init__()
-                self.requestId = "test"
-                self._names["requestId"] = "RequestId"
-                self.dic = {}
-                self.no_map = 1
-                self.sub_model = None
-                self.file = None
+        def _entity_to_dict(self, obj):
+            if type(obj) in self._dict_type:
+                obj_rtn = {k: self._entity_to_dict(v) for k, v in obj.items()}
+                return obj_rtn
+            elif type(obj) in self._list_type:
+                return [self._entity_to_dict(v) for v in obj]
+            elif type(obj) in self._base_type:
+                return obj
+            elif isinstance(obj, TeaModel):
+                prop_list = [(p, not callable(getattr(obj, p)) and p[0] != "_")
+                             for p in dir(obj)]
+                obj_rtn = {}
+                for i in prop_list:
+                    if i[1]:
+                        obj_rtn[obj._names.get(i[0]) or i[0]] = self._entity_to_dict(
+                            getattr(obj, i[0]))
+                return obj_rtn
 
-    class TestConvertSubModel(TeaModel):
-        def __init__(self):
-                super().__init__()
-                self.requestId = "subTest"
-                self._names["requestId"] = "RequestId"
-                self.id = 2
+        def to_map(self):
+            prop_list = [(p, not callable(getattr(self, p)) and p[0] != "_")
+                         for p in dir(self)]
+            pros = {}
+            for i in prop_list:
+                if i[1]:
+                    pros[self._names.get(i[0]) or i[0]] = self._entity_to_dict(
+                        getattr(self, i[0]))
+            return pros
 
-    class TestConvertMapModel(TeaModel):
+    class TestConvertModel(_TeaModel):
         def __init__(self):
-                super().__init__()
-                self.requestId = ""
-                self._names["requestId"] = "RequestId"
-                self.extendId = 0
-                self.dic = {}
-                self.sub_model = None
-        
-        
-        def to_object(self, dic):
+            super().__init__()
+            self.requestId = "test"
+            self._names["requestId"] = "RequestId"
+            self.dic = {}
+            self.no_map = 1
+            self.sub_model = None
+            self.file = None
+
+    class TestConvertSubModel(_TeaModel):
+        def __init__(self):
+            super().__init__()
+            self.requestId = "subTest"
+            self._names["requestId"] = "RequestId"
+            self.id = 2
+
+    class TestConvertMapModel(_TeaModel):
+        def __init__(self):
+            super().__init__()
+            self.requestId = ""
+            self._names["requestId"] = "RequestId"
+            self.extendId = 0
+            self.dic = {}
+            self.sub_model = None
+
+        def from_map(self, dic):
             self.requestId = dic.get("RequestId") or ""
             self.extendId = dic.get("extendId") or 0
             self.dic = dic.get("dic")
@@ -43,45 +75,45 @@ class TestClient(unittest.TestCase):
             self.sub_model.id = dic.get("sub_model").get("id")
 
     def test_get_endpoint(self):
-        self.assertEqual("test", Client.get_endpoint("test", False, ""))
+        self.assertEqual("test", client.get_endpoint("test", False, ""))
 
-        self.assertEqual("test-internal.endpoint", Client.get_endpoint("test.endpoint", False, "internal"))
+        self.assertEqual("test-internal.endpoint", client.get_endpoint("test.endpoint", False, "internal"))
 
-        self.assertEqual("oss-accelerate.aliyuncs.com", Client.get_endpoint("test", True, "accelerate"))
+        self.assertEqual("oss-accelerate.aliyuncs.com", client.get_endpoint("test", True, "accelerate"))
 
     def test_get_host(self):
-        self.assertEqual("testEndpoint", Client.get_host("", "", "testEndpoint"))
+        self.assertEqual("testEndpoint", client.get_host("", "", "testEndpoint"))
 
-        self.assertEqual("cc.CN.aliyuncs.com", Client.get_host("CC_CN", "CN", None))
+        self.assertEqual("cc.CN.aliyuncs.com", client.get_host("CC_CN", "CN", None))
 
     def test_get_signature(self):
         request = TeaRequest()
         request.query["query"] = "test"
         request.query["body"] = "test"
-        result = Client.get_signature(request, "secret")
+        result = client.get_signature(request, "secret")
         self.assertEqual("XlUyV4sXjOuX5FnjUz9IF9tm5rU=", result)
     
     def test_has_error(self):
-        self.assertTrue(Client.has_error(None))
+        self.assertTrue(client.has_error(None))
 
         dic = {}
-        self.assertFalse(Client.has_error(dic))
+        self.assertFalse(client.has_error(dic))
 
         dic["Code"] = "a"
-        self.assertTrue(Client.has_error(dic))
+        self.assertTrue(client.has_error(dic))
 
         dic["Code"] = "1"
-        self.assertTrue(Client.has_error(dic))
+        self.assertTrue(client.has_error(dic))
 
         dic["Code"] = "0"
-        self.assertFalse(Client.has_error(dic))
+        self.assertFalse(client.has_error(dic))
 
     def test_get_timestamp(self):
-        self.assertIsNotNone(Client.get_timestamp())
+        self.assertIsNotNone(client.get_timestamp())
 
-        self.assertIn("T", Client.get_timestamp())
+        self.assertIn("T", client.get_timestamp())
 
-        self.assertIn("Z", Client.get_timestamp())
+        self.assertIn("Z", client.get_timestamp())
 
     def test_convert(self):
         module_path = os.path.dirname(__file__)
@@ -94,7 +126,7 @@ class TestClient(unittest.TestCase):
             model.sub_model = sub_model
             model.file = f
             map_model = TestClient.TestConvertMapModel()
-            Client.convert(model, map_model)
+            client.convert(model, map_model)
             self.assertIsNotNone(map_model)
             self.assertEqual("test", map_model.requestId)
             self.assertEqual(0, map_model.extendId)
@@ -104,12 +136,12 @@ class TestClient(unittest.TestCase):
         dic = {}
         dic["test"] = 1
         dic["key"] = "value"
-        self.assertIsNotNone(Client.query(dic))
-        self.assertEqual("1", Client.query(dic).get("test"))
+        self.assertIsNotNone(client.query(dic))
+        self.assertEqual("1", client.query(dic).get("test"))
 
     def test_get_open_plat_form_endpoint(self):
-        self.assertEqual("openplatform.aliyuncs.com", Client.get_open_plat_form_endpoint("openplatform.aliyuncs.com", ""))
+        self.assertEqual("openplatform.aliyuncs.com", client.get_open_plat_form_endpoint("openplatform.aliyuncs.com", ""))
 
-        self.assertEqual("openplatform.aliyuncs.com", Client.get_open_plat_form_endpoint("openplatform.aliyuncs.com", "cn-hangzhou"))
+        self.assertEqual("openplatform.aliyuncs.com", client.get_open_plat_form_endpoint("openplatform.aliyuncs.com", "cn-hangzhou"))
 
-        self.assertEqual("openplatform.ap-northeast-1.aliyuncs.com", Client.get_open_plat_form_endpoint("openplatform.aliyuncs.com", "ap-northeast-1"))
+        self.assertEqual("openplatform.ap-northeast-1.aliyuncs.com", client.get_open_plat_form_endpoint("openplatform.aliyuncs.com", "ap-northeast-1"))
