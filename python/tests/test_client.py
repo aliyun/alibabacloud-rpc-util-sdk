bca2afa117f71f85dc9da9994a4309c110dba3e7
824750130 <e824750130@qq.com>
2020-05-09 11:16:28 +0800

    fix static method call rules

--- python/tests/test_client.py
+++ python/tests/test_client.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 
-from alibabacloud_rpc_util import client
+from alibabacloud_rpc_util.client import Client
 from Tea.request import TeaRequest
 from Tea.model import TeaModel
 
@@ -75,45 +75,45 @@ class TestClient(unittest.TestCase):
             self.sub_model.id = dic.get("sub_model").get("id")
 
     def test_get_endpoint(self):
-        self.assertEqual("test", client.get_endpoint("test", False, ""))
+        self.assertEqual("test", Client.get_endpoint("test", False, ""))
 
-        self.assertEqual("test-internal.endpoint", client.get_endpoint("test.endpoint", False, "internal"))
+        self.assertEqual("test-internal.endpoint", Client.get_endpoint("test.endpoint", False, "internal"))
 
-        self.assertEqual("oss-accelerate.aliyuncs.com", client.get_endpoint("test", True, "accelerate"))
+        self.assertEqual("oss-accelerate.aliyuncs.com", Client.get_endpoint("test", True, "accelerate"))
 
     def test_get_host(self):
-        self.assertEqual("testEndpoint", client.get_host("", "", "testEndpoint"))
+        self.assertEqual("testEndpoint", Client.get_host("", "", "testEndpoint"))
 
-        self.assertEqual("cc.CN.aliyuncs.com", client.get_host("CC_CN", "CN", None))
+        self.assertEqual("cc.CN.aliyuncs.com", Client.get_host("CC_CN", "CN", None))
 
     def test_get_signature(self):
         request = TeaRequest()
         request.query["query"] = "test"
         request.query["body"] = "test"
-        result = client.get_signature(request, "secret")
+        result = Client.get_signature(request, "secret")
         self.assertEqual("XlUyV4sXjOuX5FnjUz9IF9tm5rU=", result)
 
     def test_has_error(self):
-        self.assertTrue(client.has_error(None))
+        self.assertTrue(Client.has_error(None))
 
         dic = {}
-        self.assertFalse(client.has_error(dic))
+        self.assertFalse(Client.has_error(dic))
 
         dic["Code"] = "a"
-        self.assertTrue(client.has_error(dic))
+        self.assertTrue(Client.has_error(dic))
 
         dic["Code"] = "1"
-        self.assertTrue(client.has_error(dic))
+        self.assertTrue(Client.has_error(dic))
 
         dic["Code"] = "0"
-        self.assertFalse(client.has_error(dic))
+        self.assertFalse(Client.has_error(dic))
 
     def test_get_timestamp(self):
-        self.assertIsNotNone(client.get_timestamp())
+        self.assertIsNotNone(Client.get_timestamp())
 
-        self.assertIn("T", client.get_timestamp())
+        self.assertIn("T", Client.get_timestamp())
 
-        self.assertIn("Z", client.get_timestamp())
+        self.assertIn("Z", Client.get_timestamp())
 
     def test_convert(self):
         module_path = os.path.dirname(__file__)
@@ -126,7 +126,7 @@ class TestClient(unittest.TestCase):
             model.sub_model = sub_model
             model.file = f
             map_model = TestClient.TestConvertMapModel()
-            client.convert(model, map_model)
+            Client.convert(model, map_model)
             self.assertIsNotNone(map_model)
             self.assertEqual("test", map_model.requestId)
             self.assertEqual(0, map_model.extendId)
@@ -136,15 +136,15 @@ class TestClient(unittest.TestCase):
         dic = {}
         dic["test"] = 1
         dic["key"] = "value"
-        self.assertIsNotNone(client.query(dic))
-        self.assertEqual("1", client.query(dic).get("test"))
+        self.assertIsNotNone(Client.query(dic))
+        self.assertEqual("1", Client.query(dic).get("test"))
 
     def test_get_open_plat_form_endpoint(self):
         self.assertEqual("openplatform.aliyuncs.com",
-                         client.get_open_plat_form_endpoint("openplatform.aliyuncs.com", ""))
+                         Client.get_open_plat_form_endpoint("openplatform.aliyuncs.com", ""))
 
         self.assertEqual("openplatform.aliyuncs.com",
-                         client.get_open_plat_form_endpoint("openplatform.aliyuncs.com", "cn-hangzhou"))
+                         Client.get_open_plat_form_endpoint("openplatform.aliyuncs.com", "cn-hangzhou"))
 
         self.assertEqual("openplatform.ap-northeast-1.aliyuncs.com",
-                         client.get_open_plat_form_endpoint("openplatform.aliyuncs.com", "ap-northeast-1"))
+                         Client.get_open_plat_form_endpoint("openplatform.aliyuncs.com", "ap-northeast-1"))
