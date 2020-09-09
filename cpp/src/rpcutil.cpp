ea9075bd7a2c5df889a9df0634fc48d1a5b566c4
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-09-09 10:05:55 +0800

    rpc-util for c++ development

--- cpp/src/rpcutil.cpp
+++ cpp/src/rpcutil.cpp
@@ -1,5 +1,3 @@
-// This file is auto-generated, don't edit it. Thanks.
-
 #include <alibabacloud/rpcutil.hpp>
 #include <boost/any.hpp>
 #include <boost/exception/all.hpp>
@@ -10,39 +8,39 @@
 using namespace Darabonba;
 using namespace std;
 
-string Alibabacloud_RPCUtil::Client::getEndpoint(string endpoint, bool serverUse, string endpointType){
-  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented');
-}
-
-string Alibabacloud_RPCUtil::Client::getHost(string productId, string regionId, string endpoint){
-  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented');
-}
-
-string Alibabacloud_RPCUtil::Client::getSignature(Request request, string secret){
-  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented');
-}
-
-string Alibabacloud_RPCUtil::Client::getSignatureV1(map<string, string> signedParams, string method, string secret){
-  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented');
-}
-
-bool Alibabacloud_RPCUtil::Client::hasError(map<string, boost::any> obj){
-  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented');
-}
-
-string Alibabacloud_RPCUtil::Client::getTimestamp(){
-  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented');
-}
-
-void Alibabacloud_RPCUtil::Client::convert(Model body, Model content){
-  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented');
-}
-
-map<string, string> Alibabacloud_RPCUtil::Client::query(map<string, boost::any> filter){
-  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented');
-}
-
-string Alibabacloud_RPCUtil::Client::getOpenPlatFormEndpoint(string endpoint, string regionId){
-  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented');
-}
+//string Alibabacloud_RPCUtil::Client::getEndpoint(string endpoint, bool serverUse, string endpointType){
+//  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented'));
+//}
+//
+//string Alibabacloud_RPCUtil::Client::getHost(string productId, string regionId, string endpoint){
+//  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented'));
+//}
+//
+//string Alibabacloud_RPCUtil::Client::getSignature(Request request, string secret){
+//  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented'));
+//}
+//
+//string Alibabacloud_RPCUtil::Client::getSignatureV1(map<string, string> signedParams, string method, string secret){
+//  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented'));
+//}
+//
+//bool Alibabacloud_RPCUtil::Client::hasError(map<string, boost::any> obj){
+//  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented'));
+//}
+//
+//string Alibabacloud_RPCUtil::Client::getTimestamp(){
+//  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented'));
+//}
+//
+//void Alibabacloud_RPCUtil::Client::convert(Model body, Model content){
+//  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented'));
+//}
+//
+//map<string, string> Alibabacloud_RPCUtil::Client::query(map<string, boost::any> filter){
+//  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented'));
+//}
+//
+//string Alibabacloud_RPCUtil::Client::getOpenPlatFormEndpoint(string endpoint, string regionId){
+//  BOOST_THROW_EXCEPTION(boost::exception('Un-implemented'));
+//}
 
