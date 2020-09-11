6adc24e3fc6c2156a40853c86c1dd4f2d10ea6dd
824750130 <e824750130@qq.com>
2020-09-11 14:54:07 +0800

    support flatten

--- cpp/tests/main.cpp
+++ cpp/tests/main.cpp
@@ -12,8 +12,7 @@ int main(int argc, char **argv) {
   return RUN_ALL_TESTS();
 }
 
-class TestModel: public Darabonba::Model
-{
+class TestModel : public Darabonba::Model {
 public:
   void validate() override {
     cout << "test validate";
@@ -30,25 +29,22 @@ TEST(tests, getEndpoint) {
             Alibabacloud_RPCUtil::Client::getEndpoint(endpoint, true, "accelerate"));
 }
 
-TEST(tests, getHost)
-{
+TEST(tests, getHost) {
   ASSERT_EQ("testEndpoint", Client::getHost("", "", "testEndpoint"));
   ASSERT_EQ("cc.cn.aliyuncs.com", Client::getHost("CC_CN", "CN", ""));
 }
 
-TEST(tests, getSignature)
-{
+TEST(tests, getSignature) {
   Darabonba::Request TeaRequest;
   map<string, string> query = {
-    {"query", "test"},
-    {"body", "test"}
+      {"query", "test"},
+      {"body", "test"}
   };
   TeaRequest.query = query;
   ASSERT_EQ("XlUyV4sXjOuX5FnjUz9IF9tm5rU=", Client::getSignature(TeaRequest, "secret"));
 }
 
-TEST(tests, getSignatureV1)
-{
+TEST(tests, getSignatureV1) {
   map<string, string> query = {
       {"query", "test"},
       {"body", "test"}
@@ -56,8 +52,7 @@ TEST(tests, getSignatureV1)
   ASSERT_EQ("XlUyV4sXjOuX5FnjUz9IF9tm5rU=", Client::getSignatureV1(query, "GET", "secret"));
 }
 
-TEST(tests, hasError)
-{
+TEST(tests, hasError) {
 //  ASSERT_TRUE()
   map<string, boost::any> m = {};
   ASSERT_FALSE(Client::hasError(m));
@@ -68,13 +63,11 @@ TEST(tests, hasError)
   ASSERT_TRUE(Client::hasError(m1));
 }
 
-TEST(tests, getTimestamp)
-{
+TEST(tests, getTimestamp) {
   ASSERT_EQ(20, Client::getTimestamp().size());
 }
 
-TEST(tests, convert)
-{
+TEST(tests, convert) {
   TestModel iModel;
   TestModel oModel;
   string name = "name";
@@ -86,8 +79,7 @@ TEST(tests, convert)
   ASSERT_EQ(test, boost::any_cast<string>(oModel.get("test")));
 }
 
-TEST(tests, query)
-{
+TEST(tests, query) {
   const map<string, boost::any> m = {
       {"str_test", "test"},
       {"int_test", 1}
@@ -95,14 +87,33 @@ TEST(tests, query)
   map<string, string> result = Client::query(m);
   ASSERT_EQ("test", result.at("str_test"));
   ASSERT_EQ("1", result.at("int_test"));
+
+  vector<boost::any> sl = {1, 2};
+  map<string, boost::any> sub_map_fd = {
+      {"int_test", 2},
+      {"str_test", "test"}
+  };
+  map<string, boost::any> fd = {
+      {"first_map_map", sub_map_fd},
+      {"first_map_list", sl},
+      {"int_test", 2},
+      {"str_test", "test"}
+  };
+
+  map<string, string> res = Client::query(fd);
+  ASSERT_EQ("1", res.at("first_map_list.1"));
+  ASSERT_EQ("2", res.at("first_map_list.2"));
+  ASSERT_EQ("2", res.at("first_map_map.int_test"));
+  ASSERT_EQ("test", res.at("first_map_map.str_test"));
+  ASSERT_EQ("2", res.at("int_test"));
+  ASSERT_EQ("test", res.at("str_test"));
 }
 
-TEST(tests, getOpenPlatFormEndpoint)
-{
+TEST(tests, getOpenPlatFormEndpoint) {
   ASSERT_EQ(
       "openplatform.aliyuncs.com",
       Client::getOpenPlatFormEndpoint("openplatform.aliyuncs.com", "")
-      );
+  );
 
   ASSERT_EQ(
       "openplatform.aliyuncs.com",
