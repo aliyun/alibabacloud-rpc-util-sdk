0d212842cb948d2bdb0427b572a7ba97f7350293
824750130 <e824750130@qq.com>
2020-09-24 15:58:51 +0800

    parameters use shared_ptr

--- cpp/include/alibabacloud/rpcutil.hpp
+++ cpp/include/alibabacloud/rpcutil.hpp
@@ -4,6 +4,7 @@
 #define ALIBABACLOUD_RPCUTIL_H_
 
 #include <boost/any.hpp>
+#include <boost/shared_ptr.hpp>
 #include <darabonba/core.hpp>
 #include <iostream>
 #include <map>
@@ -11,20 +12,28 @@
 using namespace Darabonba;
 using namespace std;
 
+
 namespace Alibabacloud_RPCUtil {
 class Client {
 public:
-  static string getEndpoint(string *endpoint, bool *serverUse,
-                            string *endpointType);
-  static string getHost(string *productId, string *regionId, string *endpoint);
-  static string getSignature(Request *request, string *secret);
-  static string getSignatureV1(map<string, string> *signedParams,
-                               string *method, string *secret);
-  static bool hasError(map<string, boost::any> *obj);
+  static string getEndpoint(shared_ptr<string> endpoint,
+                            shared_ptr<bool> serverUse,
+                            shared_ptr<string> endpointType);
+  static string getHost(shared_ptr<string> productId,
+                        shared_ptr<string> regionId,
+                        shared_ptr<string> endpoint);
+  static string getSignature(shared_ptr<Request> request,
+                             shared_ptr<string> secret);
+  static string getSignatureV1(shared_ptr<map<string, string>> signedParams,
+                               shared_ptr<string> method,
+                               shared_ptr<string> secret);
+  static bool hasError(shared_ptr<map<string, boost::any>> obj);
   static string getTimestamp();
-  static void convert(Model *body, Model *content);
-  static map<string, string> query(map<string, boost::any> *filter);
-  static string getOpenPlatFormEndpoint(string *endpoint, string *regionId);
+  static void convert(shared_ptr<Model>& body,
+                      shared_ptr<Model>& content);
+  static map<string, string> query(shared_ptr<map<string, boost::any>> filter);
+  static string getOpenPlatFormEndpoint(shared_ptr<string> endpoint,
+                                        shared_ptr<string> regionId);
 
   Client(){};
   ~Client(){};
